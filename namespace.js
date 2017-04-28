53ce9e539cdec872606553b44298ff2f3a37941f
= <jiahong@gengee.cn>
2017-04-28 18:10:21 +0800

    socket.io learning: project initilization

---
+++ namespace.js
@@ -0,0 +1,32 @@
+const io = require('socket.io').listen(4000);
+io.sockets.on('connection',function(socket){
+  socket.on('join',function(data){
+    socket.username = data.username;
+    socket.broadcast.emit('join',{
+      username: data.username,
+      socket: socket.id
+    });
+    socket.on('publicPing',function(){
+      socket.broadcast.emit('publicPing',{username: socket.username});
+    });
+    socket.on('privatePing',function(data){
+      io.sockets.connected[data.socket].emit('ping',{username: socket.username,priv: true});
+    });
+  });
+});
+
+io.of('/vip').on('connection',function(socket){
+  socket.on('join',function(data){
+    socket.username = data.username;
+    socket.broadcast.emit('join',{
+      username: data.username,
+      socket: socket.id
+    });
+    socket.on('piing',function(){
+      socket.broadcast.emit('piing',{username:socket.username});
+    });
+    socket.on('privatePing',function(data){
+      io.of('/vip').connected[data.socket].emit('piing',{username: socket.username,priv: true});
+    });
+  });
+});
